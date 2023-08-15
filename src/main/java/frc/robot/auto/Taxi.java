5c25260385c5b741716b389fe43f7528b21a74b2
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2023-08-14 21:18:50 -0400

    PathPlanner

---
+++ src/main/java/frc/robot/auto/Taxi.java
@@ -0,0 +1,30 @@
+package frc.robot.auto;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.NavXGyro;
+import frc.robot.subsystems.swerve.AutoBase;
+import frc.robot.subsystems.swerve.SwerveBase;
+import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+
+public class Taxi extends AutoBase {
+    public Taxi(SwerveBase swerve) {
+        super(swerve);
+        PathPlannerTrajectory taxi = PathPlanner.loadPath("taxi", 4.0, 3.0);
+        PPSwerveControllerCommand command = baseSwerveCommand(taxi);
+        PathPlannerState initialState = taxi.getInitialState();
+
+        addCommands(
+                new InstantCommand(() -> NavXGyro.getInstance().resetGyro()),
+                new InstantCommand(
+                        () -> swerve.resetOdometry(new Pose2d(initialState.poseMeters.getTranslation(),
+                                initialState.holonomicRotation))),
+                command);
+
+    }
+
+
+}
