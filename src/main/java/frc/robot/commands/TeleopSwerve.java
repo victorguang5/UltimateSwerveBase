c4151042257bd0a9d5431e552b4f4e4d78001012
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2023-09-10 16:26:23 -0400

    SpeedCutoff

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -18,14 +18,23 @@ public class TeleopSwerve extends CommandBase {
     private DoubleSupplier strafeSup;
     private DoubleSupplier rotationSup;
     private BooleanSupplier robotCentricSup;
+    private BooleanSupplier speedCutoffSup;
+    private Boolean speedCutoffVal = false;
 
-    public TeleopSwerve(SwerveBase s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
+    public TeleopSwerve(
+            SwerveBase s_Swerve,
+            DoubleSupplier translationSup,
+            DoubleSupplier strafeSup,
+            DoubleSupplier rotationSup,
+            BooleanSupplier speedCutoffSup,
+            BooleanSupplier robotCentricSup) {
         this.s_Swerve = s_Swerve;
         addRequirements(s_Swerve);
 
         this.translationSup = translationSup;
         this.strafeSup = strafeSup;
         this.rotationSup = rotationSup;
+        this.speedCutoffSup = speedCutoffSup;
         this.robotCentricSup = robotCentricSup;
     }
 
@@ -35,16 +44,18 @@ public class TeleopSwerve extends CommandBase {
         double translationVal = MathUtil.applyDeadband(translationSup.getAsDouble(), Constants.stickDeadband);
         double strafeVal = MathUtil.applyDeadband(strafeSup.getAsDouble(), Constants.stickDeadband);
         double rotationVal = MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.stickDeadband);
+        speedCutoffVal = speedCutoffSup.getAsBoolean() ? !speedCutoffVal : speedCutoffVal;
         SmartDashboard.putNumber("translationVal", translationVal);
         SmartDashboard.putNumber("strafeVal", strafeVal);
         SmartDashboard.putNumber("rotationVal", rotationVal);
+        SmartDashboard.putBoolean("Speed Cut Off", speedCutoffVal);
 
         /* Drive */
         s_Swerve.drive(
-            new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed),
-            rotationVal * Constants.Swerve.maxAngularVelocity,
-            !robotCentricSup.getAsBoolean(),
-            true
+                new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed).times( speedCutoffVal ? 0.5 : 1),
+                rotationVal * Constants.Swerve.maxAngularVelocity,
+                !robotCentricSup.getAsBoolean(),
+                true
         );
     }
 }
